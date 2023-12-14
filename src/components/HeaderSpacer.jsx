326c3d8e6a2c001b5dfbf2dfd1b668c06b476b55
kukulauren <kukulauren1997@gmail.com>
2023-12-15 00:00:21 +0630

    first-commit-hero-section

---
+++ src/components/HeaderSpacer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const HeaderSpacer = () => {
+  return (
+    <div className="h-12 header-spacer" id="headerSpacer" >
+        </div>
+  )
+}
+
+export default HeaderSpacer
