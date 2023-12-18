7742ee81981e300afc8799ad35aff40ee9ce792e
kukulauren <kukulauren1997@gmail.com>
2023-12-18 11:47:25 +0630

    resolve-error

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import GuideSection from "./components/GuideSection";
 import CardSection from "./components/CardSection";
 import Section1 from "./components/Section1";
 import Footer from "./components/Footer";
-import Section1 from './components/Section1'
 import Article from './components/Article'
 const App = () => { 
   return (
