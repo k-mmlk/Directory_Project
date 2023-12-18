a953aa518fdb7dc666fc8b285e141a3b5dc8df7e
kukulauren <kukulauren1997@gmail.com>
2023-12-18 11:09:13 +0630

    responsive-for-phone

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 
 import React from 'react'
 import Section1 from './components/Section1'
-import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
       <Section1/>
-      <Footer />
       
     </div>
   );
